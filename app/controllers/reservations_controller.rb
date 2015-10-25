1584868d136ee83e1596e14b53a0ba9525f8b9d8
TiffanyD <tdarnick@outlook.com>
2015-10-25 01:00:35 +0000

    rsvp

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,4 +6,16 @@ class ReservationsController < ApplicationController
 	def new
 		@reservation=Reservation.new
 	end
+
+	def create
+		Reservation.create(reservation_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def reservation_params
+		params.require(:reservation).permit(:name, :date, :mode)
+	end
+
 end
